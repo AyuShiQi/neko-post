490beaddec6f11940fdb7d0ad1bcaf304b200d0a
Desu <673464228@qq.com>
2023-09-16 20:19:53 +0800

    feat: header logo

--- src/renderer/components/MainHeader.vue
+++ src/renderer/components/MainHeader.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="neko-main-header">
     <div class="neko-main-header__left">
-
+      <div class="neko-main-header__left__logo">
+        <img src="../assets/neko-logo.png" alt="logo">
+      </div>
+      <div class="option-list">
+        <span class="iconfont icon-daohang"></span>
+      </div>
     </div>
     <div class="neko-main-header__center">
       center
@@ -31,15 +36,54 @@
   background-color: var(--neko-bg-color);
   justify-content: space-between;
   align-items: center;
-  // 可拖拽窗口
+  user-select: none;
   // -webkit-app-region: drag;
 
   .neko-main-header__left {
-    width: 100px;
+    display: flex;
+    width: 70px;
     height: 100%;
+    padding-left: 12px;
+    align-items: center;
+    box-sizing: border-box;
+    justify-content: space-between;
+
+    .neko-main-header__left__logo {
+      width: 18px;
+      height: 18px;
+      // 可拖拽窗口，但会屏蔽所有鼠标事件
+      -webkit-app-region: drag;
+
+      img {
+        width: 100%;
+        height: 100%;
+        user-select: none;
+        -webkit-user-drag: none;
+      }
+    }
+
+    .option-list {
+      display: flex;
+      width: 24px;
+      height: 24px;
+      justify-content: center;
+      align-items: center;
+      border-radius: 5px;
+      cursor: pointer;
+
+      &:hover {
+        color: rgb(255, 255, 255);
+        background-color: var(--neko-bg-color);
+      }
+
+      .iconfont {
+        font-size: 18px;
+      }
+    }
   }
 
   .neko-main-header__center {
+    // 可拖拽窗口，但会屏蔽所有鼠标事件
     -webkit-app-region: drag;
     flex: 1;
   }
