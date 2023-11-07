d30ee0de146decc43d4ffb4c95894a9a4057823d
Desu <673464228@qq.com>
2023-11-07 22:12:08 +0800

    feat: 删除项目逻辑

--- src/renderer/Window/home-window/SettingView.vue
+++ src/renderer/Window/home-window/SettingView.vue
@@ -1,9 +1,100 @@
 <template>
-  <div></div>
+  <div class="neko-apis-setting">
+    <vi-form class="neko-form" @submit="handleTitleSubmit">
+      <template v-slot="{ submit }">
+        <vi-form-item
+        :rules="[
+          {
+            rule: /^\S{4,32}$/,
+            info: '项目名称应由4-32位非空格字符组成'
+          }
+        ]">
+          <div class="neko-form-item">
+            <vi-input name="pname" v-model="pname" type="button"></vi-input>
+            <vi-button :disabled="noModified" color="green" @click="submit">修改项目名</vi-button>
+          </div>
+        </vi-form-item>
+      </template>
+    </vi-form>
+    <vi-button class="del-proj-btn" color="red" @click="deleteDialog = true">删除该项目</vi-button>
+    <vi-dialog v-model="openTitleDialog" @sure="handleTitleSure">
+      是否确认修改项目 {{ profileStore.projectList.target?.pname }} 标题为 {{ pname }}
+    </vi-dialog>
+    <vi-dialog v-model="deleteDialog" @sure="handleDeleteSure">
+      是否删除项目 {{ profileStore.projectList.target?.pname }} ？
+    </vi-dialog>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue'
+import { useProfileStore } from '@/renderer/store'
+import { updateProjectTitle, deleteProject } from '@/renderer/network/proj'
+import { ViMessage } from 'viog-ui'
+const profileStore = useProfileStore()
+
+// 待修改的
+const pname = ref()
+const openTitleDialog = ref(false)
+const deleteDialog = ref(false)
+const noModified = computed(() => pname.value === profileStore.projectList.target.pname)
+
+function handleTitleSubmit (resMap: any, res: boolean, { getSubmitFeedback }) {
+  if (!res || noModified.value) return
+  const proj = profileStore.findProjectWithPname(pname.value)
+  if (proj) {
+    const feedbackMap = new Map<string, string>()
+    feedbackMap.set('pname', '项目名已存在')
+    getSubmitFeedback(feedbackMap)
+  } else openTitleDialog.value = true
+}
+
+function handleTitleSure () {
+  const { token, uid, pid } = profileStore
+  updateProjectTitle(token, uid, pid, pname.value).then(val => {
+    if (val.code === 200) {
+      profileStore.updateProjectList()
+      ViMessage.append('更新成功', 2000)
+    } else {
+      ViMessage.append('更新失败', 2000)
+    }
+  })
+}
+
+function handleDeleteSure () {
+  const { token, uid, pid } = profileStore
+  deleteProject(token, uid, pid).then(val => {
+    if (val.code === 200) {
+      profileStore.updateProjectList()
+      profileStore.pid = undefined
+      ViMessage.append('删除成功', 2000)
+    } else {
+      ViMessage.append('删除失败', 2000)
+    }
+  })
+}
+
+onMounted(() => {
+  pname.value = profileStore.projectList.target.pname
+})
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+.neko-apis-setting {
+  width: 100%;
+  height: 100%;
+  padding: 16px;
+  box-sizing: border-box;
+
+  .neko-form {
+    margin-bottom: 24px;
+
+    .neko-form-item {
+      display: flex;
+      align-items: center;
+      gap: 16px;
+      --vi-background-color-deep: var(--neko-content-bg-color);
+    }
+  }
+}
 </style>
--- src/renderer/network/proj.ts
+++ src/renderer/network/proj.ts
@@ -26,16 +26,60 @@ export function getProjectList (token: string, uid: string) {
  * @returns 
  */
 export function createProject (token: string, uid: string, pname: string) {
-    // console.log(username, password)
-    return getAxios<any>({
-      method: 'post',
-      url: '/proj/create',
-      headers: {
-        token
-      },
-      data: {
-        uid,
-        pname
-      }
-    })
-  }
+  // console.log(username, password)
+  return getAxios<any>({
+    method: 'post',
+    url: '/proj/create',
+    headers: {
+      token
+    },
+    data: {
+      uid,
+      pname
+    }
+  })
+}
+
+/**
+ * 更新项目标题
+ * @param token 
+ * @param uid 
+ * @param pid 
+ * @param npname 
+ * @returns 目标项目
+ */
+export function updateProjectTitle (token: string, uid: string, pid: string, npname: string) {
+  return getAxios<any>({
+    method: 'post',
+    url: '/proj/update',
+    headers: {
+      token
+    },
+    data: {
+      uid,
+      pid,
+      npname
+    }
+  })
+}
+
+/**
+ * 删除项目
+ * @param token
+ * @param uid
+ * @param pid
+ * @returns
+ */
+export function deleteProject (token: string, uid: string, pid: string) {
+  return getAxios<any>({
+    method: 'post',
+    url: '/proj/delete',
+    headers: {
+      token
+    },
+    data: {
+      uid,
+      pid
+    }
+  })
+}
